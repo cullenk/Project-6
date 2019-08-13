19584ceec9561f73103bffc3cfe0ecc9314a758f
Cullen Kuch <cullenkuch@gmail.com>
2019-08-12 21:00:51 -0400

    Phrase Display

--- app.js
+++ app.js
@@ -21,13 +21,29 @@ const phrases = [
 'one ring to rule them all'
 ];
 
-//Randomly chooses one of the phrases and plits all of the characters into an array
+//Randomly chooses one of the phrases and splits all of the characters into an array of letters
 
 function getRandomPhraseAsArray(arr){
-  const getRandomPhrase = phrases[Math.floor(Math.random() * phrases.length)].split(" ");
-  return getRandomPhrase;
+  const getRandomPhrase = arr[Math.floor(Math.random() * arr.length)]; //Targets the array and randomly selects one of the phrases
+  const phraseCharacters = getRandomPhrase.split(''); //Splits the phrase that was chosen into separate characters
+  return phraseCharacters; //returns the separate characters, stored in the phraseCharacters value
 }
 
-//Call the function to produce the new string
+//Function to take that new string of characters and display them to the screen
 
-getRandomPhraseAsArray(phrases);
+function addPhraseToDisplay(arr){
+  for (let i = 0; i < arr.length; i += 1) {
+      const listItem = document.createElement('li');
+      listItem.textContent = arr[i];
+      const ul = phrase.firstElementChild;
+      ul.appendChild(listItem);
+
+      if(phraseCharacters[i] !== ' '){
+        phraseCharacters[i].className = "letter";
+      }
+  }
+}
+
+//Call the function to produce the new string of characters using phrases as the parameter, store it into the phraseArray constant. Then call the seconds function to display that value to the screen
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
