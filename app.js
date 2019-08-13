1e9e9846500d7a1a1d8220a3a7a27fc9af9321cb
Cullen Kuch <cullenkuch@gmail.com>
2019-08-13 11:19:17 -0400

    Click function

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ const missed = 0;
 const reset = document.getElementsByClassName('btn__reset')[0];
 const overlay = document.getElementById('overlay');
 
-//Main screen disappears into the game after clicking "start game"
+
+//Main screen disappears into the game after clicking "start game".
 
 reset.addEventListener('click', () => {
   overlay.style.display = 'none';
 });
 
-//Array of phrases for game to choose from
+//Array of phrases for game to choose from.
 
 const phrases = [
 'life finds a way',
@@ -33,17 +34,42 @@ function getRandomPhraseAsArray(arr){
 
 function addPhraseToDisplay(arr){
   for (let i = 0; i < arr.length; i += 1) {
-      const listItem = document.createElement('li');
-      listItem.textContent = arr[i];
-      const ul = phrase.firstElementChild;
-      ul.appendChild(listItem);
+      const listItem = document.createElement('li'); // Cycles through the array of returned characters, turns them into list items.
+      listItem.textContent = arr[i]; // Assigns those list items the text content value of the original characters
+      const ul = phrase.firstElementChild; // Targets the first child element of the ".phrase" class (which is the ul) and assigns it a value of ul.
+      ul.appendChild(listItem); // adds a new child element to that ul, which is the list item we created with the stored phrase letter values
 
-      if(phraseCharacters[i] !== ' '){
-        phraseCharacters[i].className = "letter";
+      if(arr[i] !== ' '){
+        listItem.className = "letter"; // If the new item is not a space (which makes it a letter), give it the styling from ".letter".
+      } else {
+        listItem.style.width = '2em'; // Or if it's an actual space, widen the space so we can visually tell the different phrase words apart.
       }
   }
 }
 
-//Call the function to produce the new string of characters using phrases as the parameter, store it into the phraseArray constant. Then call the seconds function to display that value to the screen
+//Call the function to produce the new string of characters using "phrases" as the parameter, store it into the phraseArray constant. Then call the second function to display that value to the screen
 const phraseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
+
+//Create a function to check if the letters the player guesses match the ones from the phrase
+
+  function checkLetter(e){
+  const letter = document.getElementsByClassName('letter'); //Finds all of the items with the class of letter which we created above. Stores them into the "letter" constant
+  for (let i = 0; i < letter.length; i += 1) { // Loops through each letter in the phrase array
+    if (e.target === 'letter'){ // if the button clicked matches a letter in the array...
+      e.target.className = "show"; // add a class name of ".show" to that letter
+      var chosenLetter = document.getElementsByClassName('show'); // Now find all the elements that have the added class name and store it in chosenLetter.
+      return chosenLetter;
+    } else { // If the button/letter clicked doesn't match one in the phrase, return a null value.
+      return null;
+     }
+   }
+ }
+
+//Utilize the above function by adding a click handler when the user actually clicks on a letter guess
+
+qwerty.addEventListener('click', (e) => {
+  e.target.className = 'chosen';
+});
+
+const letterFound = checkLetter(e);
